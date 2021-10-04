856790de48c2d71677d3b47c1cac5179bcb5d38f
Selcuk Arikan <selcukarikan1301@gmail.com>
2021-10-04 21:58:18 +0200

    C02_VerifiyPageTitle

---
+++ C03_VerifyPageTitle.java
@@ -0,0 +1,7 @@
+package tests.day01;
+
+public class C03_VerifyPageTitle {
+    //Yeni bir Class olusturalim. (VerifyTitle)
+    //Amazon ana sayfasina gidelim . https://www.amazon.com/
+    //Sayfa basliginin (title) “amazon” oldugunu dogrulayin. (verify)
+}
